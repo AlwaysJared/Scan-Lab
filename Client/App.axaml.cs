0ffe15c2a6deb3665a3674771b91ee813c32ce32
AlwaysJared <jared.orr1209@gmail.com>
2025-04-11 14:40:35 -0400

    added demo logic to client app (add to other apps)

--- Client/App.axaml.cs
+++ Client/App.axaml.cs
@@ -1,7 +1,11 @@
+using System;
+using System.Threading.Tasks;
 using Avalonia;
+using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 using Client.Services;
+using Client.Tools;
 using Client.ViewModels;
 using Client.Views;
 
@@ -17,15 +21,31 @@ public class App : Application
         AvaloniaXamlLoader.Load(this);
     }
 
-    public override void OnFrameworkInitializationCompleted()
+    public override async void OnFrameworkInitializationCompleted()
     {
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
+            // //Demo functionality (SHOULD BE REMOVED IN V1 RELEASE)
+            // // Set expiration date and time (UTC)
+            //
+            // var expirationDate = new DateTime(2025, 4, 10, 2, 0, 0, DateTimeKind.Utc);
+            // var currentDate = DateTime.UtcNow;
+
+            // if (currentDate > expirationDate)
+            // {
+            //     desktop.MainWindow = new Window { Height = 0, Width = 0 };
+            //     await UiTools.ShowMessageAsync("Demo Expired", "This demo version of the Scan Lab app is expired. Please contact the developer to receive the full version", UiTools.MessageType.Error);
+            //     Environment.Exit(0);
+            //     return;
+            // }
+
             desktop.MainWindow = new MainWindow
             {
-                DataContext = new MainWindowViewModel(ApiService,ScannerService)
+                DataContext = new MainWindowViewModel(ApiService, ScannerService)
             };
         }
+
+
 
         base.OnFrameworkInitializationCompleted();
     }
